f1bea6fb9978b521e59fa606441b9865a9a0bcb3
viniciuskauann <vinicius.7921@aluno.pr.senac.br>
2025-08-08 21:16:20 -0300

    adicionei uma função de logout

--- exercicios/Login/login.js
+++ exercicios/Login/login.js
@@ -2,3 +2,8 @@ function boasVindas(nomeUsuario, loginUsuario)  {
     console.log(`Seja bem vindo ${nomeUsuario}`);
     console.log(`Login efetuado com sucesso! ${loginUsuario}`);
 } 
+
+
+function Logout(){
+    console.log("Logout efetuado com sucesso até mais");
+}
