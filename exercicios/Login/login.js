f06af67297a54174aeb75b32575a7e883ca8893a
GustavooTech <gustavosilva3424@gmail.com>
2025-08-08 21:16:50 -0300

    Implementei a função de Logout

--- exercicios/Login/login.js
+++ exercicios/Login/login.js
@@ -1,4 +1,9 @@
-function boasVindas(nomeUsuario, loginUsuario)  {
+function boasVindas(nomeUsuario)  {
     console.log(`Seja bem vindo ${nomeUsuario}`);
-    console.log(`Login efetuado com sucesso! ${loginUsuario}`);
+    console.log(`Login efetuado com sucesso!`);
 } 
+
+function logout(nomeUsuario) {
+    console.log(`Logout feito com sucesso! Até mais ${nomeUsuario}`);
+    
+}
